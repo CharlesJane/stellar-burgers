5b7956a0d348de3146a5996b6d5bda0faf60b8f5
Kate Polenova <double.none@yandex.ru>
2026-03-31 12:50:44 +0300

    Добавлен роутинг основных страниц, защищенных страниц (пока без реализации), и резервный путь для несуществующих страниц

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,22 @@
-import { ConstructorPage } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
 import { Preloader } from '@ui';
+
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   /** TODO: взять переменные из стора */
@@ -12,22 +25,36 @@ const App = () => {
   const error = null;
 
   return (
-    <div className={styles.app}>
-      <AppHeader />
-      {isIngredientsLoading ? (
-        <Preloader />
-      ) : error ? (
-        <div className={`${styles.error} text text_type_main-medium pt-4`}>
-          {error}
-        </div>
-      ) : ingredients.length > 0 ? (
-        <ConstructorPage />
-      ) : (
-        <div className={`${styles.title} text text_type_main-medium pt-4`}>
-          Нет игредиентов
-        </div>
-      )}
-    </div>
+    <Router>
+      <div className={styles.app}>
+        <AppHeader />
+        {isIngredientsLoading ? (
+          <Preloader />
+        ) : error ? (
+          <div className={`${styles.error} text text_type_main-medium pt-4`}>
+            {error}
+          </div>
+        ) : ingredients.length > 0 ? (
+          <Routes>
+            <Route path='/' element={<ConstructorPage />} />
+            <Route path='/feed' element={<Feed />} />
+            {/* Защищенные пути */}
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/forgot-password' element={<ForgotPassword />} />
+            <Route path='/reset-password' element={<ResetPassword />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/profile/orders' element={<ProfileOrders />} />
+            {/* Резервный путь */}
+            <Route path='*' element={<NotFound404 />} />
+          </Routes>
+        ) : (
+          <div className={`${styles.title} text text_type_main-medium pt-4`}>
+            Нет игредиентов
+          </div>
+        )}
+      </div>
+    </Router>
   );
 };
 
