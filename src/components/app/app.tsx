876f9c80b97adb3d8f76b044ffc37fdeb0ceabb1
Kate Polenova <double.none@yandex.ru>
2026-04-02 16:36:32 +0300

    Откат к второму коммиту с роутингом, запуталась в реализации. Начала заново - переписала роутинг для основных страниц, реализована навигация в шапке

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,3 @@
-import '../../index.css';
-import styles from './app.module.css';
-
-import { AppHeader } from '@components';
-import { Preloader } from '@ui';
-
 import {
   ConstructorPage,
   Feed,
@@ -15,8 +9,14 @@ import {
   ProfileOrders,
   NotFound404
 } from '@pages';
+import { Modal, OrderInfo, IngredientDetails } from '@components';
+import '../../index.css';
+import styles from './app.module.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import { AppHeader } from '@components';
+import { Preloader } from '@ui';
 
 const App = () => {
   /** TODO: взять переменные из стора */
@@ -25,36 +25,32 @@ const App = () => {
   const error = null;
 
   return (
-    <Router>
-      <div className={styles.app}>
-        <AppHeader />
-        {isIngredientsLoading ? (
-          <Preloader />
-        ) : error ? (
-          <div className={`${styles.error} text text_type_main-medium pt-4`}>
-            {error}
-          </div>
-        ) : ingredients.length > 0 ? (
-          <Routes>
-            <Route path='/' element={<ConstructorPage />} />
-            <Route path='/feed' element={<Feed />} />
-            {/* Защищенные пути */}
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/forgot-password' element={<ForgotPassword />} />
-            <Route path='/reset-password' element={<ResetPassword />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/profile/orders' element={<ProfileOrders />} />
-            {/* Резервный путь */}
-            <Route path='*' element={<NotFound404 />} />
-          </Routes>
-        ) : (
-          <div className={`${styles.title} text text_type_main-medium pt-4`}>
-            Нет игредиентов
-          </div>
-        )}
-      </div>
-    </Router>
+    <div className={styles.app}>
+      <AppHeader />
+      {isIngredientsLoading ? (
+        <Preloader />
+      ) : error ? (
+        <div className={`${styles.error} text text_type_main-medium pt-4`}>
+          {error}
+        </div>
+      ) : true ? (
+        <Routes>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/orders' element={<ProfileOrders />} />
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+      ) : (
+        <div className={`${styles.title} text text_type_main-medium pt-4`}>
+          Нет игредиентов
+        </div>
+      )}
+    </div>
   );
 };
 
