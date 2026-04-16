d990044f1e0b29b51368cd10ee11562b58afb86e
Kate Polenova <double.none@yandex.ru>
2026-04-16 21:40:13 +0300

    Написаны тесты на отправку заказа, cypress без ошибок

--- cypress/e2e/constructor/constructor.cy.tsx
+++ cypress/e2e/constructor/constructor.cy.tsx
@@ -1,9 +1,10 @@
 import type { TIngredient, TOrder, TUser } from '../../../src/utils/types';
 
-describe('Конструктор бургера', () => {
+describe('Проверка корректности работы конструктора бургера', () => {
   let ingredients: TIngredient[];
   let orderResponse: any;
 
+  // Подгружаем моки, токен
   beforeEach(() => {
     cy.fixture('ingredients.json').then((data) => {
       ingredients = data.data;
@@ -31,63 +32,30 @@ describe('Конструктор бургера', () => {
     cy.wait('@getIngredients');
   });
 
+  // Очищаем токен и куки
   afterEach(() => {
     cy.clearCookie('accessToken');
     window.localStorage.removeItem('refreshToken');
   });
 
-  it('Добавление ингредиента из списка в конструктор по клику', () => {
-    const bun = ingredients.find((i) => i.type === 'bun')!;
-    const main = ingredients.find((i) => i.type === 'main')!;
+  // Тесты
 
-    cy.wait('@getIngredients');
-    cy.wait('@getUser');
-
-    // Ждём видимости контейнера ингредиента
-    cy.get(`[data-cy="ingredient-${bun._id}"]`).should('be.visible');
-
-    // Ищем кнопку внутри контейнера ингредиента
-    cy.get(`[data-cy="ingredient-${bun._id}"]`)
-      .find('button')
-      .should('be.visible')
-      .click();
-
-    // Проверка: булка появилась сверху
-    cy.get('[data-cy="bun-top"]', { timeout: 10000 })
-      .should('be.visible')
-      .and('contain', bun.name);
-
-    // ПРОВЕРКА: булка появилась снизу (новый шаг)
-    cy.get('[data-cy="bun-bottom"]', { timeout: 10000 })
-      .should('be.visible')
-      .and('contain', bun.name);
-
-    // Добавление начинки
-    cy.get(`[data-cy="ingredient-${main._id}"]`)
-      .find('button')
-      .should('be.visible')
-      .click();
-    cy.get('[data-cy="main-section"]', { timeout: 10000 })
-      .should('be.visible')
-      .and('contain', main.name);
-  });
-
-  it('Закрытие модального окна по крестику', () => {
+  // Тесты на функциональность модалки
+  it('Открытие и закрытие модального окна по крестику', () => {
     const ingredient = ingredients[0];
 
     cy.get(`[data-cy="ingredient-${ingredient._id}"]`).click();
     cy.get('#modals [data-cy="modal"]').should('be.visible');
-    cy.get('[data-cy="modal-title"]').should('contain', 'Описание ингредиента');
 
     cy.get('#modals [data-cy="close-modal"]').click();
     cy.url().should('include', '/');
     cy.get('#modals [data-cy="modal"]').should('not.exist');
   });
 
-  it('Закрытие модального окна по оверлею', () => {
+  it('Открытие и закрытие модального окна по оверлею', () => {
     const ingredient = ingredients[0];
 
-    cy.visit('/'); // возвращаем страницу к исходному состоянию
+    cy.visit('/');
     cy.wait('@getIngredients');
 
     cy.get(`[data-cy="ingredient-${ingredient._id}"]`).click();
@@ -98,46 +66,58 @@ describe('Конструктор бургера', () => {
     cy.get('#modals [data-cy="modal"]').should('not.exist');
   });
 
-  // it('Создание заказа: сборка, оформление, проверка и очистка конструктора', () => {
-  //   const bun = ingredients.find((i) => i.type === 'bun')!;
-  //   const main = ingredients.find((i) => i.type === 'main')!;
-  //   const orderNumber = orderResponse.order.number;
+  // Объединенное: тест на добавление ингредиентов в конструктор, тест на отправку заказа
+  it('Добавление ингредиента из списка в конструктор по клику, оформление и отправка заказа на сервер', () => {
+    const bun = ingredients.find((i) => i.type === 'bun')!;
+    const main = ingredients.find((i) => i.type === 'main')!;
+    const orderNumber = orderResponse.order.number;
 
-  //   // 1. Сборка бургера
-  //   cy.get(`[data-cy="ingredient-${bun._id}"]`).click();
-  //   cy.get(`[data-cy="ingredient-${main._id}"]`).click(); // Начинка
+    cy.wait('@getIngredients');
+    cy.wait('@getUser');
 
-  //   cy.get('[data-cy="bun-top"]', { timeout: 10000 })
-  //     .should('be.visible')
-  //     .and('contain', bun.name);
-  //   cy.get('[data-cy="bun-bottom"]', { timeout: 10000 })
-  //     .should('be.visible')
-  //     .and('contain', bun.name);
-  //   cy.get('[data-cy="main-section"]', { timeout: 10000 })
-  //     .should('be.visible')
-  //     .and('contain', main.name);
+    cy.get(`[data-cy="ingredient-${bun._id}"]`).should('be.visible');
 
-  //   // 2. Оформление заказа
-  //   cy.get('[data-cy="order-button"]').click();
-  //   cy.wait('@createOrder').then((interception) => {
-  //     // Проверка существования ответа
-  //     expect(interception.response).to.exist;
-  //     // Дополнительная проверка: ответ API содержит ожидаемый номер
-  //     expect(interception.response?.body.order.number).to.equal(orderNumber);
-  //   });
+    // Добавление булок
+    cy.get(`[data-cy="ingredient-${bun._id}"]`)
+      .find('button')
+      .should('be.visible')
+      .click();
+    cy.contains('[data-cy="bun-top"]', bun.name, { matchCase: false }).should(
+      'be.visible'
+    );
+    cy.contains('[data-cy="bun-bottom"]', bun.name, {
+      matchCase: false
+    }).should('be.visible');
 
-  //   // 3. Проверка модального окна
-  //   cy.get('[data-cy="modal"]').should('be.visible');
-  //   cy.get('[data-cy="order-number"]').should('contain', orderNumber);
+    // Добавление начинки
+    cy.get(`[data-cy="ingredient-${main._id}"]`)
+      .find('button')
+      .should('be.visible')
+      .click();
+    cy.contains('[data-cy="main-section"]', main.name, {
+      matchCase: false
+    }).should('be.visible');
 
-  //   // 4. Закрытие модального окна
-  //   cy.get('[data-cy="close-modal"]').click();
-  //   cy.get('[data-cy="modal"]').should('not.be.visible');
+    // Оформление заказа
+    cy.get('[data-cy="order-button"]').click();
+    cy.wait('@createOrder').then((interception) => {
+      expect(interception.response).to.exist;
+      expect(interception.response?.body.order.number).to.equal(orderNumber);
+    });
 
-  //   // 5. Проверка пустого конструктора
-  //   cy.get('[data-cy="no-bun-top"]').should('be.visible');
-  //   cy.get('[data-cy="no-bun-bottom"]').should('be.visible');
-  //   cy.get('[data-cy="no-ingredients"]').should('be.visible');
-  //   cy.get('[data-cy="total-price"]').should('have.text', '0');
-  // });
+    // Проверка корректности номера заказа в модалке
+    cy.get('[data-cy="modal"]').should('be.visible');
+    cy.contains('[data-cy="order-number"]', orderNumber.toString(), {
+      matchCase: false
+    }).should('be.visible');
+    cy.get('#modals [data-cy="close-modal"]').click();
+    cy.url().should('include', '/');
+    cy.get('#modals [data-cy="modal"]').should('not.exist');
+
+    // Проверка очищения конструктора
+    cy.get('[data-cy="no-bun-top"]').should('be.visible');
+    cy.get('[data-cy="no-bun-bottom"]').should('be.visible');
+    cy.get('[data-cy="no-ingredients"]').should('be.visible');
+    cy.get('[data-cy="total-price"]').should('have.text', '0');
+  });
 });
