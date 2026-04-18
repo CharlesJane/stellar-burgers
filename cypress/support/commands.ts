e7d19a12aa1d53c5e12243a6f57ea103cafe228e
Kate Polenova <double.none@yandex.ru>
2026-04-18 18:47:21 +0300

    Поправлены все ошибки и пожелания, кроме вынесения команд. Cypress выдает ошибку по собранному dist, моя версия не проходит. Возможно, проблема в версиях, но пока не разобралась.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,78 +1,79 @@
-import type { TIngredient } from '../../src/utils/types';
+export {};
+// import type { TIngredient } from '../../src/utils/types';
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       openIngredientModal(ingredient: TIngredient): void;
+//       closeModal(): void;
+//       addIngredientToConstructor(ingredient: TIngredient): void;
+//       verifyOrderModalAndClose(orderNumber: number): void;
+//       verifyConstructorCleared(): void;
+//     }
+//   }
+// }
 
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      openIngredientModal(ingredient: TIngredient): void;
-      closeModal(): void;
-      addIngredientToConstructor(ingredient: TIngredient): void;
-      verifyOrderModalAndClose(orderNumber: number): void;
-      verifyConstructorCleared(): void;
-    }
-  }
-}
+// Cypress.Commands.add('openIngredientModal', (ingredient: TIngredient) => {
+//   cy.get(`[data-cy="ingredient-${ingredient._id}"]`)
+//     .as('ingredientElement')
+//     .click()
+//     .get('#modals [data-cy="modal"]')
+//     .as('modalWindow')
+//     .should('be.visible');
+// });
 
-Cypress.Commands.add('openIngredientModal', (ingredient: TIngredient) => {
-  cy.get(`[data-cy="ingredient-${ingredient._id}"]`)
-    .as('ingredientElement')
-    .click()
-    .get('#modals [data-cy="modal"]')
-    .as('modalWindow')
-    .should('be.visible');
-});
+// Cypress.Commands.add('closeModal', () => {
+//   cy.get('@modalWindow')
+//     .find('[data-cy="close-modal"]')
+//     .as('closeModalButton')
+//     .should('be.visible')
+//     .click()
+//     .get('@modalWindow')
+//     .should('not.exist');
+// });
 
-Cypress.Commands.add('closeModal', () => {
-  cy.get('@modalWindow')
-    .find('[data-cy="close-modal"]')
-    .as('closeModalButton')
-    .should('be.visible')
-    .click()
-    .get('@modalWindow')
-    .should('not.exist');
-});
+// Cypress.Commands.add(
+//   'addIngredientToConstructor',
+//   (ingredient: TIngredient) => {
+//     cy.get(`[data-cy="ingredient-${ingredient._id}"]`)
+//       .find('button')
+//       .as(`${ingredient.type}Button`)
+//       .should('be.visible')
+//       .click();
+//   }
+// );
 
-Cypress.Commands.add(
-  'addIngredientToConstructor',
-  (ingredient: TIngredient) => {
-    cy.get(`[data-cy="ingredient-${ingredient._id}"]`)
-      .find('button')
-      .as(`${ingredient.type}Button`)
-      .should('be.visible')
-      .click();
-  }
-);
+// Cypress.Commands.add('verifyOrderModalAndClose', (orderNumber: number) => {
+//   cy.get('#modals [data-cy="modal"]')
+//     .as('orderModal')
+//     .should('be.visible')
+//     .contains('[data-cy="order-number"]', orderNumber.toString(), {
+//       matchCase: false
+//     })
+//     .as('orderNumberElement')
+//     .should('be.visible')
+//     .get('@orderModal')
+//     .find('[data-cy="close-modal"]')
+//     .as('closeOrderModalButton')
+//     .should('be.visible')
+//     .click()
+//     .get('@orderModal')
+//     .should('not.exist')
+//     .url()
+//     .should('include', '/');
+// });
 
-Cypress.Commands.add('verifyOrderModalAndClose', (orderNumber: number) => {
-  cy.get('#modals [data-cy="modal"]')
-    .as('orderModal')
-    .should('be.visible')
-    .contains('[data-cy="order-number"]', orderNumber.toString(), {
-      matchCase: false
-    })
-    .as('orderNumberElement')
-    .should('be.visible')
-    .get('@orderModal')
-    .find('[data-cy="close-modal"]')
-    .as('closeOrderModalButton')
-    .should('be.visible')
-    .click()
-    .get('@orderModal')
-    .should('not.exist')
-    .url()
-    .should('include', '/');
-});
-
-Cypress.Commands.add('verifyConstructorCleared', () => {
-  cy.get('[data-cy="no-bun-top"]')
-    .as('noBunTop')
-    .should('be.visible')
-    .get('[data-cy="no-bun-bottom"]')
-    .as('noBunBottom')
-    .should('be.visible')
-    .get('[data-cy="no-ingredients"]')
-    .as('noIngredients')
-    .should('be.visible')
-    .get('[data-cy="total-price"]')
-    .as('totalPrice')
-    .should('have.text', '0');
-});
+// Cypress.Commands.add('verifyConstructorCleared', () => {
+//   cy.get('[data-cy="no-bun-top"]')
+//     .as('noBunTop')
+//     .should('be.visible')
+//     .get('[data-cy="no-bun-bottom"]')
+//     .as('noBunBottom')
+//     .should('be.visible')
+//     .get('[data-cy="no-ingredients"]')
+//     .as('noIngredients')
+//     .should('be.visible')
+//     .get('[data-cy="total-price"]')
+//     .as('totalPrice')
+//     .should('have.text', '0');
+// });
+/// <reference types="cypress" />
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,16 +1,16 @@
-import type { TIngredient } from '../../src/utils/types';
+// import type { TIngredient } from '../../src/utils/types';
 
 import './commands';
-import 'cypress';
+// import 'cypress';
 
-declare global {
-  namespace Cypress {
-    interface Chainable<Subject = any> {
-      openIngredientModal(ingredient: TIngredient): Chainable<void>;
-      closeModal(): Chainable<void>;
-      addIngredientToConstructor(ingredient: TIngredient): Chainable<void>;
-      verifyOrderModalAndClose(orderNumber: number): Chainable<void>;
-      verifyConstructorCleared(): Chainable<void>;
-    }
-  }
-}
+// declare global {
+//   namespace Cypress {
+//     interface Chainable<Subject = any> {
+//       openIngredientModal(ingredient: TIngredient): Chainable<void>;
+//       closeModal(): Chainable<void>;
+//       addIngredientToConstructor(ingredient: TIngredient): Chainable<void>;
+//       verifyOrderModalAndClose(orderNumber: number): Chainable<void>;
+//       verifyConstructorCleared(): Chainable<void>;
+//     }
+//   }
+// }
